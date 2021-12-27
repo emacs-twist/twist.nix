94c410721b8f398eb4fac97de8084bd541c5f660
Akira Komamura <akira.komamura@gmail.com>
2021-12-27 15:28:46 +0900

    chore: Drop pre-commit-hooks from flake.nix
    I am now more interested in reducing boilerplates.
    
    You can use akirak/git-hooks to install a typical set of hooks:
    
      nix develop github:akirak/git-hooks

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "Source-based Emacs Lisp build machinery";
 
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  inputs.pre-commit-hooks = {
-    url = "github:cachix/pre-commit-hooks.nix/50cfce93606c020b9e69dce24f039b39c34a4c2d";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
-  };
   inputs.elisp-helpers = {
     url = "github:akirak/nix-elisp-helpers";
     flake = false;
@@ -18,44 +9,9 @@
     url = "github:talyz/fromElisp";
     flake = false;
   };
-  inputs.flake-no-path.url = "github:akirak/flake-no-path";
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , pre-commit-hooks
-    , ...
-    } @ inputs:
-    ({
-      overlay = import ./pkgs inputs;
-    }
-    //
-    (flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
+  outputs = { ... } @ inputs:
     {
-      checks = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            nixpkgs-fmt.enable = true;
-            nix-linter.enable = true;
-            flake-no-path = {
-              enable = true;
-              name = "Ensure that flake.lock does not contain a local path";
-              entry = "${
-                inputs.flake-no-path.packages.${system}.flake-no-path
-              }/bin/flake-no-path";
-              files = "flake\.lock$";
-              pass_filenames = true;
-            };
-          };
-        };
-      };
-      devShell = nixpkgs.legacyPackages.${system}.mkShell {
-        inherit (self.checks.${system}.pre-commit-check) shellHook;
-      };
-    })));
+      overlay = import ./pkgs inputs;
+    };
 }
