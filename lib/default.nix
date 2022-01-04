a5888e86b44eab251ccfd1e865ab6146020ceda7
Akira Komamura <akira.komamura@gmail.com>
2022-01-04 16:10:12 +0900

    fix!: Change parseSetup to require options
    Considering the fact that setup is a set of helper macros with
    extensibility, the user is likely to customize many of its aspects.
    I think there is no point in enforcing one set of default options.

--- lib/default.nix
+++ lib/default.nix
@@ -5,15 +5,11 @@ let
   fromElisp = import inputs.fromElisp {
     pkgs = { inherit lib; };
   };
-
-  parseSetup' = import ../pkgs/build-support/elisp/parseSetup.nix {
-    inherit lib fromElisp;
-  };
 in
 {
-  parseSetup = parseSetup' { };
-  # Like parseSetup, but allows customization.
-  inherit parseSetup';
+  parseSetup = import ../pkgs/build-support/elisp/parseSetup.nix {
+    inherit lib fromElisp;
+  };
 
   parseUsePackages = import ../pkgs/build-support/elisp/parseUsePackages.nix {
     inherit lib fromElisp;
