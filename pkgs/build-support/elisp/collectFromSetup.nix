ffb2f3436bf90b85961e2e6d009b6939db6b9f52
Akira Komamura <akira.komamura@gmail.com>
2022-01-04 16:25:53 +0900

    refactor: Factor out for handling multiple keywords

---
+++ pkgs/build-support/elisp/collectFromSetup.nix
@@ -0,0 +1,35 @@
+{ lib
+}:
+keyword:
+with builtins;
+let
+  go = { data, rest } @ acc: fields:
+    {
+      data = data ++ lib.pipe fields [
+        (map (field:
+          if isList field && length field > 0 && head field == keyword
+          then tail field
+          else null))
+        (filter lib.isList)
+      ];
+      rest = (lib.pipe fields [
+        (filter isList)
+        concatLists
+        (filter isList)
+      ]) ++ rest;
+    };
+
+  recurse = { rest, ... } @ acc:
+    if rest == [ ]
+    then removeAttrs acc [ "rest" ]
+    else recurse (go (acc // { rest = [ ]; }) rest);
+in
+blocks:
+lib.pipe blocks [
+  (foldl' go {
+    data = [ ];
+    rest = [ ];
+  })
+  recurse
+  (attrs: attrs.data)
+]
--- pkgs/build-support/elisp/parseSetup.nix
+++ pkgs/build-support/elisp/parseSetup.nix
@@ -7,37 +7,16 @@
 with builtins;
 string:
 let
-  blocks = fromElisp.fromElisp string;
+  setups = lib.pipe (fromElisp.fromElisp string) [
+    (filter (block: head block == "setup"))
+    (map tail)
+  ];
 
-  go = { elispPackages, rest } @ acc: fields:
-    {
-      elispPackages = elispPackages ++ lib.pipe fields [
-        (map (field:
-          if isList field && length field > 0 && head field == packageKeyword
-          then tail field
-          else null))
-        (filter lib.isList)
-        lib.concatLists
-      ];
-      rest = (lib.pipe fields [
-        (filter isList)
-        concatLists
-        (filter isList)
-      ]) ++ rest;
-    };
-
-  recurse = { rest, ... } @ acc:
-    if rest == [ ]
-    then removeAttrs acc [ "rest" ]
-    else recurse (go (acc // { rest = [ ]; }) rest);
+  collect = import ./collectFromSetup.nix { inherit lib; };
 in
-lib.pipe blocks [
-  (filter (block: head block == "setup"))
-  (map tail)
-  (foldl' go {
-    elispPackages = [ ];
-    rest = [];
-  })
-  recurse
-  (lib.mapAttrs (_: lib.unique))
-]
+{
+  elispPackages = lib.pipe (collect packageKeyword setups) [
+    lib.concatLists
+    lib.unique
+  ];
+}
