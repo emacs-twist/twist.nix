ac200ab38aab57796242e9236ff7d33cb37e03f2
Akira Komamura <akira.komamura@gmail.com>
2022-01-09 22:42:40 +0900

    feat!: Replace the version status with a check derivation
    This is intended for integration with `nix flake check`.

--- pkgs/emacs/tools/check-versions.nix
+++ pkgs/emacs/tools/check-versions.nix
@@ -1,11 +1,13 @@
 { lib
 , emacsVersion
 , builtinLibraries
+, emptyFile
+, runCommandLocal
 }:
 packageInputs:
 let
   inherit (builtins) mapAttrs all hasAttr elem length match head
-    substring lessThan concatLists sort;
+    substring lessThan concatLists sort concatStringsSep attrNames;
 
   srcDateString = src: substring 0 8 src.lastModifiedDate;
 
@@ -42,38 +44,49 @@ let
     }) [ "isDateVersion" ];
 
   filterErrors = lib.filterAttrs (_: { satisfied, ... }: !satisfied);
+
+  status = rec {
+    summary = lib.pipe errors [
+      (lib.mapAttrsToList (requiredBy: lib.mapAttrsToList
+        (ename: status: {
+          inherit ename requiredBy;
+        } // status)))
+      concatLists
+      (lib.groupBy ({ ename, ... }: ename))
+      (mapAttrs (ename: statuses: {
+        current = (head statuses).actual;
+        # Showing the source date may be useful, but maybe later.
+        #
+        # lastModifiedDate =
+        #   if hasAttr ename packageInputs
+        #   then packageInputs.${ename}.src.lastModifiedDate or null
+        #     # unavailable
+        #   else null;
+        required = lib.pipe (lib.catAttrs "required" statuses) [
+          lib.naturalSort
+          lib.last
+        ];
+        details =
+          map (status: removeAttrs status [ "actual" "ename" "satisfied" ]) statuses;
+      }))
+    ];
+    errors = lib.pipe packages [
+      (mapAttrs (_: filterErrors))
+      (lib.filterAttrs (_: attrs: attrs != { }))
+    ];
+    packages = lib.pipe packageInputs [
+      (mapAttrs (_: { packageRequires, ... }:
+        mapAttrs dependencyStatus packageRequires))
+    ];
+  };
 in
-rec {
-  summary = lib.pipe errors [
-    (lib.mapAttrsToList (requiredBy: lib.mapAttrsToList
-      (ename: status: {
-        inherit ename requiredBy;
-      } // status)))
-    concatLists
-    (lib.groupBy ({ ename, ... }: ename))
-    (mapAttrs (ename: statuses: {
-      current = (head statuses).actual;
-      # Showing the source date may be useful, but maybe later.
-      #
-      # lastModifiedDate =
-      #   if hasAttr ename packageInputs
-      #   then packageInputs.${ename}.src.lastModifiedDate or null
-      #     # unavailable
-      #   else null;
-      required = lib.pipe (lib.catAttrs "required" statuses) [
-        lib.naturalSort
-        lib.last
-      ];
-      details =
-        map (status: removeAttrs status [ "actual" "ename" "satisfied" ]) statuses;
-    }))
-  ];
-  errors = lib.pipe packages [
-    (mapAttrs (_: filterErrors))
-    (lib.filterAttrs (_: attrs: attrs != { }))
-  ];
-  packages = lib.pipe packageInputs [
-    (mapAttrs (_: { packageRequires, ... }:
-      mapAttrs dependencyStatus packageRequires))
-  ];
-}
+if status.errors == { }
+then emptyFile
+else runCommandLocal "emacs-deps-error" {
+  passthru = status;
+} ''
+  echo >&2 "ERROR: Some packages require updates: ${
+    concatStringsSep " " (attrNames status.summary)
+  }"
+  exit 1
+''
