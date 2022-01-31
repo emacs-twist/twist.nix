2033f0bf189db7aedb6f034738ff607350ae1f1a
Akira Komamura <6270544+akirak@users.noreply.github.com>
2022-01-31 21:20:55 +0900

    chore: Add a configuration template (#17)

---
+++ template/inventories.nix
@@ -0,0 +1,41 @@
+inputs:
+[
+  {
+    type = "melpa";
+    path = ./recipes;
+  }
+  {
+    type = "elpa";
+    path = inputs.gnu-elpa.outPath + "/elpa-packages";
+    core-src = inputs.emacs.outPath;
+    auto-sync-only = true;
+  }
+  {
+    name = "melpa";
+    type = "melpa";
+    path = inputs.melpa.outPath + "/recipes";
+    exclude = [
+      "bbdb"
+    ];
+  }
+  {
+    type = "elpa";
+    path = inputs.nongnu.outPath + "/elpa-packages";
+    exclude = [
+      "org-contrib"
+    ];
+  }
+  {
+    type = "archive";
+    url = "https://elpa.gnu.org/packages/";
+  }
+  {
+    type = "archive";
+    url = "https://elpa.nongnu.org/nongnu/";
+  }
+  {
+    name = "emacsmirror";
+    type = "gitmodules";
+    path = inputs.epkgs.outPath + "/.gitmodules";
+  }
+]
